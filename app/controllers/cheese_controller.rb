7abc0d6c88fd5e17dc871bc94cbdca6fe682e7a9
hoonkim <hoonk9387@gmail.com>
2022-12-29 10:58:11 -0600

    Created basic rails api server. Added route, controller with index action and view index.html.erb.

---
+++ app/controllers/cheese_controller.rb
@@ -0,0 +1,11 @@
+class CheeseController < ApplicationController
+    # Mapped to get /cheese.
+    # Default looks for folder of same controller name 
+    # with view file index.html.erb in app/views.
+    
+    # To return JSON use render method.
+    def index
+        render json: { hello: "Cheeseburger!!!", today: "Thursday"}
+    end
+
+end
---
+++ config/routes.rb
@@ -0,0 +1,6 @@
+Rails.application.routes.draw do
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  # GET request to /cheese map to cheese#index
+  get "cheese", to: "cheese#index"
+end
